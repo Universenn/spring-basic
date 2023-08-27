4ea8d3171723c8dee6488cb28b6644fa4a28564d
Universenn <dnwndhks98@gmail.com>
2023-08-27 15:24:31 +0900

    [test] xml 으로 빈 등록

---
+++ src/test/java/com/example/springbasic/xml/XmlConfigContext.java
@@ -0,0 +1,19 @@
+package com.example.springbasic.xml;
+
+import com.example.springbasic.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+
+public class XmlConfigContext {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new
+                GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService",
+                MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
